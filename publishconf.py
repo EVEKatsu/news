d01d724c7bdd4fab8bc08931e33074225b0161c3
Omochin <omochin7@gmail.com>
2018-09-12 20:27:26 +0900

    Fix SITEURL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://evekatsu.github.io/blog'
+SITEURL = 'https://evekatsu.github.io/news'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
