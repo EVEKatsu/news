8fe1e9ff4abe0dbe45a633f7a227d6bba41d3e10
Omochin <omochin7@gmail.com>
2019-01-16 23:15:08 +0900

    Add 2018-12 ranking

--- ranking_article_generator.py
+++ ranking_article_generator.py
@@ -102,7 +102,7 @@ def main():
     month = str(int(sys.argv[2]))
 
     try:
-        with urllib.request.urlopen('https://evekatsu.github.io/ranking/%d/%d/players_information.json' % (year, month)) as url:
+        with urllib.request.urlopen('https://evekatsu.github.io/ranking/%s/%s/players_information.json' % (year, month)) as url:
             players_information = json.loads(url.read().decode())
     except urllib.error.HTTPError:
         print('Error: players_information.json')
